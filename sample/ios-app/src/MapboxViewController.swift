c2e228ee18c963e173e5c92042e973c08ca072b3
Ivan Krylov <ikrylov@icerockdev.com>
2020-02-27 16:55:08 +0700

    icerockdev#19 Fix copyrighs. Remove mapbox key

--- sample/ios-app/src/MapboxViewController.swift
+++ sample/ios-app/src/MapboxViewController.swift
@@ -1,5 +1,5 @@
 /*
-* Copyright 2010 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+* Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
 */
 
 import Mapbox
